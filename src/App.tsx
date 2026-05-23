e8ce854b0d50a892aa1012043c77683f5f3917c5
Jonathan GAU <gau.jonathan@gmail.com>
2026-05-23 20:59:41 +0200

    feat: add Framer Motion page transitions with AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 import { HomePage } from '@/features/home/HomePage'
 import { DestinationPage } from '@/features/destinations/DestinationPage'
 import { ChatWidget } from '@/features/chatbot/ChatWidget'
 import { QuizModal } from '@/features/quiz/QuizModal'
+import { PageTransition } from '@/components/layout/PageTransition'
+
+function AnimatedRoutes() {
+  const location = useLocation()
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageTransition><HomePage /></PageTransition>} />
+        <Route path="/destination/:id" element={<PageTransition><DestinationPage /></PageTransition>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/destination/:id" element={<DestinationPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <AnimatedRoutes />
       <ChatWidget />
       <QuizModal />
     </BrowserRouter>
